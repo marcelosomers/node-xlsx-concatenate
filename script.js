00aac7346f1e5f730a4eae323812e4ac8290ea3f
Marcelo Somers <msomers@gmail.com>
2015-01-10 22:26:32 -0600

    Working with file names

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var fs = require("fs")
     xlsx = require("xlsx");
 
 var inputDirectory = "files/";
+var csvContent = "";
 
 fs.readdir(inputDirectory, function(err, files) {
     if (err) {
@@ -11,21 +12,50 @@ fs.readdir(inputDirectory, function(err, files) {
     // Loop over all files in the input directory
     for(var i=0, len=files.length; i < len; i++) {
 
-        // Read the file
-        var file = xlsx.readFile(inputDirectory + files[i]);
+        var fileName = files[i];
 
-        // parse file
-        to_json(file);
+        if(files[i] !== ".DS_Store") {
+            // Read the file
+            var file = xlsx.readFile(inputDirectory + files[i]);
+
+            // parse file
+            to_json(file, fileName, i);
+        }
     }
 });
 
 
-function to_json(workbook) {
+function to_json(workbook, fileName, loop) {
     var sheet_name_list = workbook.SheetNames;
     var sheet = workbook.Sheets[sheet_name_list[0]];
     var data = xlsx.utils.sheet_to_json(sheet, {header:1});
 
-    fs.appendFile("output1.txt", data, function(err) {
+    // Loop over each row in the file
+    for(var i=0; i<data.length; i++) {
+        // add the file name to every row but the header
+        if (i === 0) {
+            data[i].unshift("File Name")
+        } else {
+            data[i].unshift(fileName);  // add the file name at the beginning
+            data[i].push('\r\n');       // add a line break at the end
+        }
+
+        var row = "";
+
+        // output the properly formatted string (makes it work with comma data)
+        for (var index in data[i]) {
+            row += '"' + data[i][index] + '",';
+        }
+
+        // strip off the last trailing comma
+        row.slice(0, row.length - 1);
+
+        csvContent += row + '\r\n';
+    }
+
+
+    // Write to output1.csv
+    fs.appendFile("output1.csv", csvContent, function(err) {
         if(err) {
             throw err;
         }
